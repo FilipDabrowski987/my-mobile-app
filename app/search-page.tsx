6e37156b389cb54329421fdcb75a8cae3e9a0840
FilipDabrowski987 <filipdabrowski@vp.pl>
2025-02-02 15:28:44 +0100

    update

--- app/search-page.tsx
+++ app/search-page.tsx
@@ -3,37 +3,31 @@ import { Button, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SearchScreen() {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
         <Text style={styles.label}>Wyszukaj:</Text>
-        <Button
-          title={'Opiekuna'}
-          onPress={() => router.push('/')} />
-        <Button
-          title={'Pacjenta'}
-          onPress={() => router.push('/')} />
-        <Button
-          title={'Wróć'}
-          onPress={() => router.back()} />
+        <Button title={"Opiekuna"} onPress={() => router.push("/")} />
+        <Button title={"Podopiecznego"} onPress={() => router.push("/")} />
+        <Button title={"Wróć"} onPress={() => router.back()} />
       </View>
     </SafeAreaView>
-)}
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'lightgray',
+    alignItems: "center",
+    backgroundColor: "lightgray",
     gap: 20,
   },
   label: {
     marginTop: 20,
     fontSize: 24,
     marginBottom: 10,
-    fontWeight: '700',
-    color: '#333',
-  }
-})
+    fontWeight: "700",
+    color: "#333",
+  },
+});
